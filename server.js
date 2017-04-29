dd7606ec55a4484e4352a8181d521376b8f35797
tangollama <tangollama@gmail.com>
2017-04-29 17:07:23 -0400

    Add New Relic custom params
    Now we’re capturing data on the inquiries and downloads into New Relic.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv-safe').load();
 
 const express = require('express');
 const url = 'https://releases.hospitalrun.io';
+var newrelic = require('newrelic');
 const port = Number(process.env.PORT) || 5006;
 // https://releases.hospitalrun.io/downloads/releases/latest/macos/HospitalRun.zip
 const assetDownloadUrl = '/downloads/releases';
@@ -22,6 +23,12 @@ app.get('/updates/:asset', (request, response) => {
   const asset = request.params.asset;
   const platform = 'macos';
   const version = process.env.HOSPITALRUN_STABLE_VERSION;
+  newrelic.addCustomParameters({
+    'hr_version': version,
+    'hr_platform': platform,
+    'hr_asset': asset,
+    'hr_update_action': 'download'
+  });
   // console.log(`${url}${assetDownloadUrl}/${version}/${platform}/${asset}`);
   response.redirect(`${url}${assetDownloadUrl}/${version}/${platform}/${asset}`);
 });
@@ -30,6 +37,13 @@ app.get('/updates/:platform/:asset', (request, response) => {
   const asset = request.params.asset;
   const platform = request.params.platform;
   const version = process.env.HOSPITALRUN_STABLE_VERSION;
+  // log the update inquiry in insights
+  newrelic.addCustomParameters({
+    'hr_version': version,
+    'hr_platform': platform,
+    'hr_asset': asset,
+    'hr_update_action': 'download'
+  });
   // console.log(`${url}${assetDownloadUrl}/${version}/${platform}/${asset}`);
   response.redirect(`${url}${assetDownloadUrl}/${version}/${platform}/${asset}`);
 });
@@ -37,6 +51,15 @@ app.get('/updates/:platform/:asset', (request, response) => {
 app.get('/updates', (request, response) => {
   const latestRelease = process.env.HOSPITALRUN_STABLE_VERSION;
   const version = request.query.version;
+
+  // log the update inquiry in insights
+  newrelic.addCustomParameters({
+    'hr_version': request.query.version,
+    'hr_platform': request.query.platform,
+    'hr_update_action': (request.query.version === latestRelease),
+    'hr_update_action': 'inquiry'
+  });
+
   if (version === latestRelease) {
     response.status(204).end();
   } else {
@@ -56,6 +79,7 @@ app.get('/updates', (request, response) => {
         fullurl += 'win32x64/HospitalRun.exe';
         break;
     }
+
     response.json({
       name: `HospitalRun v${latestRelease}`,
       notes: 'The latest release of HospitalRun.',
